2ed81da25ffbca4d8f6ca46a421a53038540b153
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-11-12 23:12:43 +0000

    Finalização do CSS e início do JS

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,38 @@ btnLinkedin.addEventListener('click', () => {
 function newWindow(url) {
     window.open(url);
 }
+
+/* Integração API GitHub */
+
+function getProjects() {
+    const url = 'https://api.github.com/users/GuilhermeEduardo23/repos';
+    var loadingElement = document.getElementById('loading');
+
+    fetch(url, {
+        method: 'GET'
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            loadingElement.style.display = 'none'
+            showProjects(response)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+function showProjects(data) {
+    var listElement = document.getElementById('my-projectslist');
+
+    for(let i = 0; i < data.length; i++) {
+        let a = document.createElement('a');
+        a.href = data[i]['clone_url'];
+        a.target = '_blank';
+        a.title = data[i]['description'];
+        let linkText = document.createTextNode(data[i]['name']);
+        a.appendChild(linkText);
+        listElement.appendChild(a);
+    }
+}
+
+getProjects();
